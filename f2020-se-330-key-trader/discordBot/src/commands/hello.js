4bda30fb9e57d4038b19d14723dd3b0393616180
andrewbauer <abauer@chapman.edu>
2020-10-02 23:53:18 -0700

    second commit

--- f2020-se-330-key-trader/discordBot/src/commands/hello.js
+++ f2020-se-330-key-trader/discordBot/src/commands/hello.js
@@ -1,5 +1,7 @@
 // Tests the discord bot connection. (Responds with with "Hello, firstname lastname!")
 
+const { createUser } = require("../../../webapp/key-trader/backend/db");
+
 exports.run = async (client, message, args) => {
-    message.channel.send("Hello ${client}").catch(console.error);
+    message.channel.send("Hello " + message.author.toString() + "!").catch(console.error);
 }
